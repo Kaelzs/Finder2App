dd0f64f0253e34ef4369b92eb4685d10a223e22e
Kael <kael.zs17@gmail.com>
2020-12-09 16:20:10 +0800

    Initial Commit

---
+++ Finder2App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Finder2App
+//
+//  Created by Kael on 2020/12/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
